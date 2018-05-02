a6fa9c44442c9ccc4feadefc5584ace459c4eb37
Martin Morgenstern <mail@mmorgenstern.net>
2018-05-02 18:15:44 +0200

    Use english locale for the benchmarks

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 JVM_ARGS="-Dot.weavable=whitelist.txt"
+JVM_ARGS="$JVM_ARGS -Duser.language=en -Duser.country=US"
 JVM_ARGS="$JVM_ARGS -Xbootclasspath/a:libs/runtime/otre_min.jar"
 JVM_ARGS="$JVM_ARGS -javaagent:libs/runtime/otredyn_agent.jar"
 
