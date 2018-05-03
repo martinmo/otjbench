d086fe4915d8a079bc4f2eadd7c49eab2c9013cd
Martin Morgenstern <mail@mmorgenstern.net>
2018-05-03 17:24:58 +0200

    Use singleshot by default for the Bank benchmark

--- src/main/java/otjbench/jmh/BankBenchmark.java
+++ src/main/java/otjbench/jmh/BankBenchmark.java
@@ -1,7 +1,10 @@
 package otjbench.jmh;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
@@ -13,8 +16,10 @@ import otjbench.bank.Bank;
 import otjbench.bank.Person;
 import otjbench.bank.Transaction;
 
+@Fork(warmups = 1)
+@BenchmarkMode(Mode.SingleShotTime)
 @State(Scope.Benchmark)
-public class BankBenchmark extends BenchmarkDefaults {
+public class BankBenchmark {
 	@Param("1500")
 	int N;
 
